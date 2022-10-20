81241f8c1d183d553a55cba32be2726179cd4dac
itwithlyam <lyam.mosnier@itwithlyam.co.uk>
2022-10-20 21:09:50 +0100

    Fix compile script

--- compile.sh
+++ compile.sh
@@ -11,3 +11,8 @@ if grub-file --is-x86-multiboot myos.bin; then
 else
   echo the file is not multiboot
 fi
+
+echo Moving
+cp myos.bin isodir/boot/myos.bin
+cp grub.cfg isodir/boot/grub/grub.cfg
+grub-mkrescue -o myos.iso isodir
