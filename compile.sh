39f8ed9a301ecd61655f638c3bc5f22ac7cd77e7
itwithlyam <lyam.mosnier@itwithlyam.co.uk>
2022-10-20 20:44:34 +0100

    First

---
+++ compile.sh
@@ -0,0 +1,13 @@
+echo Assembling
+i686-elf-as boot.s -o boot.o
+echo Compiling
+i686-elf-gcc -c kernel.c -o kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
+echo Linking
+i686-elf-gcc -T linker.ld -o myos.bin -ffreestanding -O2 -nostdlib boot.o kernel.o -lgcc
+
+echo Confirming
+if grub-file --is-x86-multiboot myos.bin; then
+  echo multiboot confirmed
+else
+  echo the file is not multiboot
+fi
