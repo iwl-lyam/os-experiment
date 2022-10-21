326f6a8e8fb6ca70ab98b27eeaf371a670e53a5f
itwithlyam <lyam.mosnier@itwithlyam.co.uk>
2022-10-21 10:57:08 +0100

    Some paging

--- compile.sh
+++ compile.sh
@@ -2,19 +2,19 @@ echo Assembling
 i686-elf-as boot.s -o boot.o
 echo "Assembled ... Compiling"
 i686-elf-gcc -c kernel.c -o kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
+#i686-elf-gcc -c descriptor_tables.c -o descriptor_tables.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
 echo "Compiled ... Linking"
 i686-elf-gcc -T linker.ld -o myos.bin -ffreestanding -O2 -nostdlib boot.o kernel.o -lgcc
 
 echo "Linked ... Verifying GRUB"
 if grub-file --is-x86-multiboot myos.bin; then
-  echo multiboot confirmed
+    echo "GRUB Verified ... Moving to OS"
+    cp myos.bin isodir/boot/myos.bin
+    cp grub.cfg isodir/boot/grub/grub.cfg
+    grub-mkrescue -o myos.iso isodir
+
+    echo "OS ready to run"
 else
-  echo the file is not multiboot
+  echo "Verification Failure"
 fi
 
-echo "GRUB Verified ... Moving to OS"
-cp myos.bin isodir/boot/myos.bin
-cp grub.cfg isodir/boot/grub/grub.cfg
-grub-mkrescue -o myos.iso isodir
-
-echo "OS ready to run"
