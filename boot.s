8cdd431ec29cfc3e1d669fa1b191db1f6797f0c5
itwithlyam <lyam.mosnier@itwithlyam.co.uk>
2022-10-22 09:46:49 +0100

    NASM tests

--- boot.s
+++ boot.s
@@ -64,8 +64,8 @@ _start:
 	; C++ features such as global constructors and exceptions will require
 	; runtime support to work as well.
 
-    call setGdt
-    call reloadSegments
+    ;call setGdt
+    ;call reloadSegments
  
 	; Enter the high-level kernel. The ABI requires the stack is 16-byte
 	; aligned at the time of the call instruction (which afterwards pushes
